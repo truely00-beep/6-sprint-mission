732d656f0f65b62664734ae64ffec9880eb38747
Jueun Lee <wndms328@naver.com>
2025-10-16 18:40:01 +0900

    add electronicProduct class

---
+++ .electroniceProduct.js
@@ -0,0 +1,29 @@
+// electronicProduct.js
+// ElectronicProduct 클래스를 정의합니다.
+
+import { Product } from "./product.js";
+
+export class ElectronicProduct extends Product {
+  _manufacturer;
+
+  constructor({
+    name,
+    description,
+    price,
+    tags = [],
+    images = [],
+    favoriteCount = 0,
+    manufacturer,
+  }) {
+    super({ name, description, price, tags, images, favoriteCount });
+
+    this._manufacturer = manufacturer;
+  }
+  getManufacturer() {
+    return this._manufacturer;
+  }
+  // 다형성: 오버라이드
+  summary() {
+    return `[전자제품] ${this.getName()} (${this.getManufacturer()}) - ${this.getPrice()}원`;
+  }
+}
