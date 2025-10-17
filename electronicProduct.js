a2cc966197923c811b87b50eabf8c189220624f3
Yeongjun <ing08114@gmail.com>
2025-10-17 11:22:37 +0900

    electronicProduct use getter ver

--- electronicProduct.js
+++ electronicProduct.js
@@ -1,4 +1,4 @@
-import { Product } from "./product.js";
+import { Product } from './product.js';
 
 export class ElectronicProduct extends Product {
   #manufacturer;
@@ -15,9 +15,13 @@ export class ElectronicProduct extends Product {
     super(name, description, price, tags, images, favoriteCount);
     this.#manufacturer = manufacturer;
   }
-  getManufacturer() {
+
+  get manufavturer() {
     return this.#manufacturer;
   }
+  // getManufacturer() {
+  //   return this.#manufacturer;
+  // }
   favorite() {
     super.favorite();
   }
