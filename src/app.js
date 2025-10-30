c57d9c1cd433bf61e1b4744c6675ab5cb805ed36
Sunyoung <d2tectiv2@naver.com>
2025-10-30 18:48:26 +0900

    article API 추가:  aricleRouter 및 app.js 수정

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import express from 'express';
 import 'express-async-errors';
 import { PORT } from './utils/constants.js';
 import productRouter from './routers/productsRouter.js';
+import articleRouter from './routers/articleRouter.js';
 import { errorHandlerMiddleware } from './middlewares/errorHandler.js';
 
 const app = express();
 app.use(express.json());
 
 app.use('/products', productRouter);
+app.use('/articles', articleRouter);
 app.use(errorHandlerMiddleware);
 
 app.listen(PORT, () => console.log('서버 시작'));
--- src/routers/ArticleRouter.js
+++ src/routers/ArticleRouter.js
@@ -1,2 +1,104 @@
 import express from 'express';
-import { prisma } from '../utils/prisma';
+import { prisma } from '../utils/prisma.js';
+import { validatePagination } from '../middlewares/paginationValidator.js';
+
+const router = express.Router();
+
+router
+  .route('/')
+  .post(async (req, res) => {
+    const data = req.body;
+    const article = await prisma.article.create({
+      data,
+    });
+    res.status(201).send(article);
+  })
+  .get(validatePagination, async (req, res) => {
+    const { sort, search } = req.query;
+    const { offset: _offset, limit: _limit } = req.paginationParams;
+
+    const orderBy = {};
+
+    if (sort === 'recent') {
+      orderBy.createdAt = 'desc';
+    }
+
+    const where = {};
+    if (search) {
+      where.OR = [{ title: { contains: search } }, { content: { contains: search } }];
+    }
+
+    const [articles, totalArticles] = await Promise.all([
+      prisma.article.findMany({
+        select: {
+          id: true,
+          title: true,
+          content: true,
+          createdAt: true,
+        },
+        where,
+        orderBy,
+        skip: _offset,
+        take: _limit,
+      }),
+
+      prisma.article.count({
+        where,
+      }),
+    ]);
+
+    if (search && totalArticles === 0) {
+      return res.status(200).json({
+        message: `${search}와 일치하는 게시물을 찾을 수 없습니다.`,
+        data: [],
+        pagination: {
+          totalItems: 0,
+          totalPages: 0,
+          currentPage: 1,
+          itemsPerPage: _limit,
+        },
+      });
+    }
+
+    const totalPages = Math.ceil(totalArticles / _limit);
+
+    res.status(200).json({
+      message: '게시판 목록을 조회했습니다.',
+      data: articles,
+      pagination: {
+        totalItems: totalArticles,
+        totalPages: totalPages,
+        currentPage: Math.floor(_offset / _limit) + 1,
+        itemsPerPage: _limit,
+      },
+    });
+  });
+
+// id 사용하는 API
+router
+  .route('/:id')
+  .get(async (req, res) => {
+    const { id } = req.params;
+    const article = await prisma.article.findUniqueOrThrow({
+      where: { id },
+    });
+    res.status(200).send(article);
+  })
+  .patch(async (req, res) => {
+    const { id } = req.params;
+    const data = req.body;
+    const article = await prisma.article.update({
+      where: { id },
+      data,
+    });
+    res.status(200).send(article);
+  })
+  .delete(async (req, res) => {
+    const { id } = req.params;
+    const article = await prisma.article.delete({
+      where: { id },
+    });
+    res.status(200).send(article);
+  });
+
+export default router;
