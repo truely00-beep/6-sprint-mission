78d705b874a60203115e36cc70212790b37eae25
오윤 <passfile2@naver.com>
2025-10-31 19:16:51 +0900

    feat: commentRouter.js생성 및 CRUD 추가, server.js에 연결

---
+++ src/routes/commentRouter.js
@@ -0,0 +1,74 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const router = express.Router();
+const prisma = new PrismaClient();
+
+router.post('/product/:productId', async (req, res) => {
+  const { productId } = req.params;
+  const { content } = req.body;
+  const data = await prisma.comment.create({
+    data: { content, productId },
+  });
+  res.status(201).json(data);
+});
+
+router.post('/article/:articleId', async (req, res) => {
+  const { articleId } = req.params;
+  const { content } = req.body;
+  const data = await prisma.comment.create({
+    data: { articleId, content },
+  });
+  res.status(201).json(data);
+});
+
+router.patch('/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = await prisma.comment.update({
+    where: { id },
+    data: req.body,
+  });
+  res.status(200).json(data);
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = await prisma.comment.delete({
+    where: { id },
+  });
+  res.status(204).json(data);
+});
+
+router.get('/product/:productId', async (req, res) => {
+  const { productId } = req.params;
+  const cursor = req.query.cursor;
+  const take = parseInt(req.query.take) || 10;
+
+  const data = await prisma.comment.findMany({
+    where: { productId },
+    take: take,
+    skip: cursor ? 1 : 0,
+    ...(cursor ? { cursor: { id: cursor } } : {}),
+    orderBy: { createdAt: 'desc' },
+    select: { id: true, content: true, createdAt: true },
+  });
+  res.status(200).json(data);
+});
+
+router.get('/article/:articleId', async (req, res) => {
+  const { articleId } = req.params;
+  const cursor = req.query.cursor;
+  const take = parseInt(req.query.take) || 10;
+
+  const data = await prisma.comment.findMany({
+    where: { articleId },
+    take: take,
+    skip: cursor ? 1 : 0,
+    ...(cursor ? { cursor: { id: cursor } } : {}),
+    orderBy: { createdAt: 'desc' },
+    select: { id: true, content: true, createdAt: true },
+  });
+  res.status(200).json(data);
+});
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import productRouters from './routes/productRouter.js';
 import articleRouters from './routes/articleRouter.js';
+import commentRouters from './routes/commentRouter.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
@@ -13,6 +14,7 @@ app.use(cors());
 
 app.use('/products', productRouters);
 app.use('/articles', articleRouters);
+app.use('/comments', commentRouters);
 
 app.listen(PORT, () => {
   console.log('Server running');
