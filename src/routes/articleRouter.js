ea87aa34b24f69a59a8cd3363cbd4d0afe9297bd
오윤 <passfile2@naver.com>
2025-11-02 23:19:32 +0900

    feat: server.js에 multer 이미지업로드 구현, uploads디렉토리 추가

--- src/routes/articleRouter.js
+++ src/routes/articleRouter.js
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 import {
   validateArticle,
   validateUpdateArticle,
-} from '../middlewares/validateArticle.js';
+} from '../middlewares/validate/validateArticle.js';
 
 const router = express.Router();
 const prisma = new PrismaClient();
--- src/server.js
+++ src/server.js
@@ -4,10 +4,12 @@ import dotenv from 'dotenv';
 import productRouters from './routes/productRouter.js';
 import articleRouters from './routes/articleRouter.js';
 import commentRouters from './routes/commentRouter.js';
+import multer from 'multer';
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 const app = express();
+const upload = multer({ dest: 'uploads/' });
 
 app.use(express.json());
 app.use(cors());
@@ -15,6 +17,13 @@ app.use(cors());
 app.use('/products', productRouters);
 app.use('/articles', articleRouters);
 app.use('/comments', commentRouters);
+app.use('/files', express.static('uploads'));
+
+app.post('/uploads', upload.single('attachment'), (req, res) => {
+  const { filename } = req.file;
+  const path = `/files/${filename}`;
+  res.status(200).json({ path });
+});
 
 app.listen(PORT, () => {
   console.log('Server running');
