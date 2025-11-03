2a77ef78065ff5f4cf305dafb1c76886366fdbc6
YM_KIM <k.youmi3263@gmail.com>
2025-11-03 13:18:55 +0900

    feat(validator): req.body 공백 시 나타나는 코드

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -1,16 +1,24 @@
+// 400 에러
+
 //중고 마켓
 const validateProduct = (req, res, next) => {
   const { name, description, price } = req.body;
   if (!name) {
-    return next(new Error('상품 이름(name)은 필수입니다'));
+    const err = new Error('상품 이름(name)은 필수입니다');
+    err.status = 400;
+    return next(err);
   }
 
   if (!description) {
-    return next(new Error('상품 설명(description)은 필수입니다.'));
+    const err = new Error('상품 설명(description)은 필수입니다.');
+    err.status = 400;
+    return next(err);
   }
 
   if (!price) {
-    return next(new Error('상품 가격(price)은 필수입니다.'));
+    const err = new Error('상품 가격(price)은 필수입니다.');
+    err.status = 400;
+    return next(err);
   }
 
   next();
@@ -21,11 +29,15 @@ const validateArticle = (req, res, next) => {
   const { title, content } = req.body;
 
   if (!title) {
-    return next(new Error('게시글 제목(title)은 필수입니다.'));
+    const err = new Error('게시글 제목(title)은 필수입니다.');
+    err.status = 400;
+    return next(err);
   }
 
   if (!content) {
-    return next(new Error('게시글 내용(content)은 필수입니다.'));
+    const err = new Error('게시글 내용(content)은 필수입니다.');
+    err.status = 400;
+    return next(err);
   }
 
   next();
@@ -36,7 +48,9 @@ const validateComment = (req, res, next) => {
   const { content } = req.body;
 
   if (!content) {
-    return next(new Error('댓글 내용(content)은 필수입니다.'));
+    const err = new Error('댓글 내용(content)은 필수입니다.');
+    err.status = 400;
+    return next(err);
   }
   next();
 };
