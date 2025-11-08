d7837cbb7652b5c8cd3ce04b92b111b8cb0d3e08
chamysj <chamysj0429@gmail.com>
2025-11-09 05:05:15 +0900

    Delete src directory

--- src/structs/articleStructs.js
+++
@@ -1,9 +0,0 @@
-import * as s from 'superstruct';
-import isUuid from 'is-uuid';
-
-export const CreateArticle = s.object({
-  title: s.size(s.string(), 1, 100),
-  content: s.size(s.string(), 1, 1200),
-});
-
-export const PatchArticle = s.partial(CreateArticle);
