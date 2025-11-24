e9a682ece040f02481a86aa3f2fb3242d01b969c
minyeok <rltkeh150@naver.com>
2025-11-24 18:48:19 +0900

    아티클 컨트롤러 함수정의 라우터파일 수정

---
+++ src/controller/articleController.js
@@ -0,0 +1,54 @@
+import { prisma } from '../utils/prismaClient.js';
+
+export class ArticleController {
+  static getArticles = async (req, res) => {
+    const { offset = 0, limit = 10, order, search } = req.query;
+    const orderByOption = {
+      recent: { createdAt: 'desc' },
+      oldest: { createdAt: 'asc' },
+    };
+    const where = search
+      ? { OR: [{ title: { contains: search } }, { content: { contains: search } }] }
+      : {};
+
+    const article = await prisma.article.findMany({
+      where,
+      orderBy: orderByOption[order] || orderByOption['recent'],
+      skip: parseInt(offset),
+      take: parseInt(limit),
+      select: { id: true, title: true, content: true, createdAt: true },
+    });
+    res.status(200).send(article);
+  };
+  static createArticle = async (req, res) => {
+    const { title, content } = req.body;
+    const article = await prisma.article.create({
+      data: { title, content },
+    });
+    res.status(201).send(article);
+  };
+  static getArticleDetail = async (req, res) => {
+    const { id } = req.params;
+    const article = await prisma.article.findUniqueOrThrow({
+      where: { id },
+      select: { id: true, title: true, content: true, createdAt: true },
+    });
+    res.status(200).send(article);
+  };
+  static patchArticle = async (req, res) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    const article = await prisma.article.update({
+      where: { id },
+      data: { title, content },
+    });
+    res.status(200).send(article);
+  };
+  static deleteArticle = async (req, res) => {
+    const { id } = req.params;
+    await prisma.article.delete({
+      where: { id },
+    });
+    res.sendStatus(204);
+  };
+}
--- src/routers/articleRouter.js
+++ src/routers/articleRouter.js
@@ -1,81 +1,24 @@
 import express from 'express';
-import { Prisma, PrismaClient } from '@prisma/client';
 import { validate } from '../middleware/validate.js';
-
-import { CreateArticle, PatchArticle } from '../structers/articleStruct.js';
-import { asyncDeleteHandler } from '../middleware/deleteHandler.js';
+import { CreateArticle, PatchArticle } from '../structs/articleStruct.js';
 import { tryCatchHandler } from '../middleware/errorhandler.js';
 import { uploadHandler } from '../middleware/upload.js';
 import multer from 'multer';
-const prisma = new PrismaClient();
+import { ArticleController } from '../controller/articleController.js';
 
 const articleRouter = express.Router();
 
 // 자유게시판 목록 조회 및 생성
 articleRouter
   .route('/')
-  .get(
-    tryCatchHandler(async (req, res) => {
-      const { offset = 0, limit = 10, order, search } = req.query;
-      let orderBy;
-      switch (order) {
-        case 'recent':
-          orderBy = { createdAt: 'desc' };
-          break;
-        case 'oldest':
-          orderBy = { createdAt: 'asc' };
-          break;
-        default:
-          orderBy = {};
-          break;
-      }
-      const where = search
-        ? { OR: [{ title: { contains: search } }, { content: { contains: search } }] }
-        : {};
-
-      const article = await prisma.article.findMany({
-        where,
-        orderBy,
-        skip: parseInt(offset),
-        take: parseInt(limit),
-        select: { id: true, title: true, content: true, createdAt: true },
-      });
-      res.status(200).send(article);
-    }),
-  )
-  .post(validate(CreateArticle), async (req, res) => {
-    const { title, content } = req.body;
-    const article = await prisma.article.create({
-      data: { title, content },
-    });
-    res.status(201).send(article);
-  });
+  .get(tryCatchHandler(ArticleController.getArticles))
+  .post(validate(CreateArticle), tryCatchHandler(ArticleController.createArticle));
 // 자유게시판 상세 조회 및 수정 및 삭제
 articleRouter
   .route('/:id')
-  .get(
-    tryCatchHandler(async (req, res) => {
-      const { id } = req.params;
-      const article = await prisma.article.findUniqueOrThrow({
-        where: { id },
-        select: { id: true, title: true, content: true, createdAt: true },
-      });
-      res.status(200).send(article);
-    }),
-  )
-  .patch(
-    validate(PatchArticle),
-    tryCatchHandler(async (req, res) => {
-      const { id } = req.params;
-      const { title, content } = req.body;
-      const article = await prisma.article.update({
-        where: { id },
-        data: { title, content },
-      });
-      res.status(200).send(article);
-    }),
-  )
-  .delete(tryCatchHandler(asyncDeleteHandler(prisma.article)));
+  .get(tryCatchHandler(ArticleController.getArticleDetail))
+  .patch(validate(PatchArticle), tryCatchHandler(ArticleController.patchArticle))
+  .delete(tryCatchHandler(ArticleController.deleteArticle));
 
 // 자유게시판 이미지 업로드
 const upload = multer({ dest: 'articlesUpload/' });
