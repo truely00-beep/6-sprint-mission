c4704b6ab1157b354659ae99a5d6d214ba9e5b48
minyeok <rltkeh150@naver.com>
2025-11-24 22:53:05 +0900

    상품클래스함수정의/상품 라우터 정리

---
+++ src/controller/productController.js
@@ -0,0 +1,73 @@
+import { prisma } from '../utils/prismaClient';
+
+export class ProductController {
+  //상품 리스트 조회
+  static getProduct = async (req, res) => {
+    const { offset = 0, limit = 10, order, search } = req.query;
+    const orderbyOption = {
+      recent: { createdAt: 'desc' },
+      oldest: { createdAt: 'asc' },
+    };
+    // const where = search ? { name || description:search} : {}; >> 첫 시도
+    const where = search
+      ? { OR: [{ name: { contains: search } }, { description: { contains: search } }] }
+      : {};
+    // 프리즈마 내에서 or 사용 방법 {OR:{[{조건1},{조건2}]}}  >  ai 활용
+    //{fieldName:{contains:...}} >> contains는 where이라는 옵션 안에있는 특정 필드등에 적용되는 더 세부적인 옵션
+    const product = await prisma.product.findMany({
+      where,
+      orderBy: orderbyOption[order] || orderbyOption['recent'],
+      skip: parseInt(offset),
+      take: parseInt(limit),
+      // include: { description: false, updatedAt: false }, >> 첫 시도, 잠재적 문제 발생 할 수도있음,prisma에서 의도한 사용방법이 아님
+      select: {
+        //findMany 도움말 참고
+        id: true,
+        name: true,
+        price: true,
+        createdAt: true,
+      },
+    });
+    res.status(200).send(product);
+  };
+  static createProduct = async (req, res) => {
+    const product = await prisma.product.create({
+      data: req.body,
+    });
+    res.send(product);
+  };
+  //상품 상세 조회
+  static getProductDetail = async (req, res) => {
+    const { id } = req.params;
+    const product = await prisma.product.findUniqueOrThrow({
+      where: { id },
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        price: true,
+        tag: true,
+        createdAt: true,
+      },
+    });
+    res.status(200).send(product);
+  };
+  //상품 정보 수정
+  static patchProduct = async (req, res) => {
+    const { id } = req.params;
+
+    const product = await prisma.product.update({
+      where: { id },
+      data: req.body,
+    });
+    res.status(200).send(product);
+  };
+  //상품 삭제
+  static deleteProduct = async (req, res) => {
+    const { id } = req.params;
+    await prisma.product.delete({
+      where: { id },
+    });
+    res.sendStatus(203);
+  };
+}
--- src/middleware/errorhandler.js
+++ src/middleware/errorhandler.js
@@ -15,7 +15,7 @@ export function tryCatchHandler(handler) {
 export function errorHandler(e, req, res, next) {
   //   (&&) 연산자는 "내가 반환하는 값이 true인가? false인가?" 에는 관심이 없음. 규칙에 따라 값을 전달. if 문이 &&연산자가 truthy값인지 falsy값인지 판단
   if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === 'P2025') {
-    return res.status(404).send({ message: '객체를 찾을 수 없음' });
+    return res.status(404).send({ message: '정보를 찾을 수 없음' });
   } else if (e.name === 'StructError') {
     console.log(e);
     return res.status(400).send({ message: '전달 사항을 조건에 맞춰 수정해주세요' });
