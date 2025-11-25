580cc2f2e48173ab3d00ef74cf9a70de5711b826
Sunyoung <d2tectiv2@naver.com>
2025-11-25 15:13:38 +0900

    추가: access, refresh token 상수 관리

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,7 @@ dotenv.config();
 export const PORT = process.env.PORT || 3000;
 export const ACCESS_TOKEN_COOKIE_NAME = process.env.ACCESS_TOKEN_COOKIE_NAME || 'accessToken';
 export const REFRESH_TOKEN_COOKIE_NAME = process.env.REFRESH_TOKEN_COOKIE_NAME || 'refreshToken';
+export const JWT_ACCESS_TOKEN_SECRET =
+  process.env.JWT_ACCESS_TOKEN_SECRET || 'jwt_access_token_secret';
+export const JWT_REFRESH_TOKEN_SECRET =
+  process.env.JWT_REFRESH_TOKEN_SECRET || 'jwt_refresh_token_secret';
