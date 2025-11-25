84c9606f652acad1af2beab2d85bcfb5b5750606
Jueun Lee <wndms328@naver.com>
2025-11-25 09:52:34 +0900

    Initialize project with express, prisma dependencies

--- src/lib/errors/NotFoundError.js
+++
@@ -1,8 +0,0 @@
-class NotFoundError extends Error {
-  constructor(modelName, id) {
-    super(`${modelName} with id ${id} not found`);
-    this.name = 'NotFoundError';
-  }
-}
-
-export default NotFoundError;
