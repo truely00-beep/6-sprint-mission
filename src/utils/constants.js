2c90b8cc463b4c8cfe8357ad0d4e0c53ee583e37
Sunyoung <d2tectiv2@naver.com>
2025-11-25 15:02:59 +0900

    추가: 쿠키 이름 상수로 관리

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,5 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
+export const ACCESS_TOKEN_COOKIE_NAME = process.env.ACCESS_TOKEN_COOKIE_NAME || 'accessToken';
+export const REFRESH_TOKEN_COOKIE_NAME = process.env.REFRESH_TOKEN_COOKIE_NAME || 'refreshToken';
