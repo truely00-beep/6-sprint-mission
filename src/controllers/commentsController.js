48f4cb7e044fd873704b0940a025b5322da1021b
Sunyoung <d2tectiv2@naver.com>
2025-11-25 20:02:10 +0900

    수정: 댓글 CRUD에 auth 적용

--- src/controllers/commentsController.js
+++ src/controllers/commentsController.js
@@ -1,96 +1,83 @@
 import { commentsService } from '../services/commentsService.js';
 
 export async function createComment(req, res, next) {
-  try {
-    const { articleId, productId } = req.params;
-    const { content } = req.body;
+  const { articleId, productId } = req.params;
+  const { content } = req.body;
+  const userId = req.user.id;
 
-    let newComment;
-    if (articleId) {
-      newComment = await commentsService.createArticleComment(articleId, content);
-    } else if (productId) {
-      newComment = await commentsService.createProductComment(productId, content);
-    } else {
-      const err = new Error('게시글 또는 상품 ID가 필요합니다.');
-      err.status = 400;
-      throw err;
-    }
-
-    res.status(201).send(newComment);
-  } catch (error) {
-    next(error);
+  let newComment;
+  if (articleId) {
+    newComment = await commentsService.createArticleComment(articleId, content, userId);
+  } else if (productId) {
+    newComment = await commentsService.createProductComment(productId, content, userId);
+  } else {
+    const err = new Error('게시글 또는 상품 ID가 필요합니다.');
+    err.status = 400;
+    throw err;
   }
+
+  res.status(201).send(newComment);
 }
 
 export async function getComments(req, res, next) {
-  try {
-    const { articleId, productId } = req.params;
-    const { cursor, limit } = req.paginationParams;
+  const { articleId, productId } = req.params;
+  const { cursor, limit } = req.paginationParams;
 
-    let result;
-    if (articleId) {
-      result = await commentsService.findCommentsByArticleId({
-        articleId,
-        cursor,
-        limit,
-      });
-    } else if (productId) {
-      result = await commentsService.findCommentsByProductId({
-        productId,
-        cursor,
-        limit,
-      });
-    } else {
-      const err = new Error('게시글 또는 상품 ID가 필요합니다.');
-      err.status = 400;
-      throw err;
-    }
-
-    res.status(200).json({
-      message: '댓글 목록을 조회했습니다.',
-      data: result.comments,
-      pagination: {
-        nextCursor: result.nextCursor,
-      },
+  let result;
+  if (articleId) {
+    result = await commentsService.findCommentsByArticleId({
+      articleId,
+      cursor,
+      limit,
     });
-  } catch (error) {
-    next(error);
+  } else if (productId) {
+    result = await commentsService.findCommentsByProductId({
+      productId,
+      cursor,
+      limit,
+    });
+  } else {
+    const err = new Error('게시글 또는 상품 ID가 필요합니다.');
+    err.status = 400;
+    throw err;
   }
+
+  res.status(200).json({
+    message: '댓글 목록을 조회했습니다.',
+    data: result.comments,
+    pagination: {
+      nextCursor: result.nextCursor,
+    },
+  });
 }
 
 export async function patchComment(req, res, next) {
-  try {
-    const { id } = req.params;
-    const { content } = req.body;
+  const { commentId } = req.params;
+  const { content } = req.body;
+  const userId = req.user.id;
 
-    if (!content) {
-      const err = new Error('수정할 내용이 비어 있습니다.');
-      err.status = 400;
-      throw err;
-    }
-
-    const updatedComment = await commentsService.updateCommentInDb(id, content);
-
-    res.status(200).json({
-      message: '댓글이 성공적으로 수정되었습니다.',
-      data: updatedComment,
-    });
-  } catch (error) {
-    next(error);
+  if (!content) {
+    const err = new Error('수정할 내용이 비어 있습니다.');
+    err.status = 400;
+    throw err;
   }
+
+  const updatedComment = await commentsService.updateCommentInDb(commentId, content, userId);
+
+  res.status(200).json({
+    message: '댓글이 성공적으로 수정되었습니다.',
+    data: updatedComment,
+  });
 }
 
 export async function deleteComment(req, res, next) {
-  try {
-    const { id } = req.params;
+  const { commentId } = req.params;
+  const userId = req.user.id;
 
-    const deletedComment = await commentsService.deleteCommentInDb(id);
+  const deletedComment = await commentsService.deleteCommentInDb(commentId, userId);
 
-    res.status(200).json({
-      message: '댓글이 성공적으로 삭제되었습니다.',
-      data: deletedComment,
-    });
-  } catch (error) {
-    next(error);
-  }
+  res.status(200).json({
+    message: '댓글이 성공적으로 삭제되었습니다.',
+    data: deletedComment,
+  });
 }
