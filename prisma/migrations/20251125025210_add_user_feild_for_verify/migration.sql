b261c0c88621f75b0a0b4b96a628d71694046b3e
minyeok <rltkeh150@naver.com>
2025-11-25 13:29:20 +0900

    add bcrypt, cookie parser,jwt

---
+++ prisma/migrations/20251125025210_add_user_feild_for_verify/migration.sql
@@ -0,0 +1,151 @@
+/*
+  Warnings:
+
+  - The primary key for the `Article` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The `id` column on the `Article` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The primary key for the `Comment` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The `id` column on the `Comment` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The `productId` column on the `Comment` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The `articleId` column on the `Comment` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The primary key for the `Order` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The `id` column on the `Order` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The primary key for the `OrderItem` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The `id` column on the `OrderItem` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The primary key for the `Product` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The `id` column on the `Product` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The `id` column on the `User` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The primary key for the `UserPreference` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The `id` column on the `UserPreference` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - A unique constraint covering the columns `[nickname]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Changed the type of `userId` on the `Comment` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `userId` on the `Order` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `orderId` on the `OrderItem` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `productId` on the `OrderItem` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Added the required column `nickname` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Changed the type of `userId` on the `UserPreference` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_articleId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Order" DROP CONSTRAINT "Order_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderItem" DROP CONSTRAINT "OrderItem_orderId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderItem" DROP CONSTRAINT "OrderItem_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "UserPreference" DROP CONSTRAINT "UserPreference_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Article" DROP CONSTRAINT "Article_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+ADD CONSTRAINT "Article_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+DROP COLUMN "productId",
+ADD COLUMN     "productId" INTEGER,
+DROP COLUMN "articleId",
+ADD COLUMN     "articleId" INTEGER,
+DROP COLUMN "userId",
+ADD COLUMN     "userId" INTEGER NOT NULL,
+ADD CONSTRAINT "Comment_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "Order" DROP CONSTRAINT "Order_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+DROP COLUMN "userId",
+ADD COLUMN     "userId" INTEGER NOT NULL,
+ADD CONSTRAINT "Order_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "OrderItem" DROP CONSTRAINT "OrderItem_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+DROP COLUMN "orderId",
+ADD COLUMN     "orderId" INTEGER NOT NULL,
+DROP COLUMN "productId",
+ADD COLUMN     "productId" INTEGER NOT NULL,
+ADD CONSTRAINT "OrderItem_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "Product" DROP CONSTRAINT "Product_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+ADD CONSTRAINT "Product_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ADD COLUMN     "nickname" TEXT NOT NULL,
+ADD COLUMN     "password" TEXT NOT NULL,
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "UserPreference" DROP CONSTRAINT "UserPreference_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+DROP COLUMN "userId",
+ADD COLUMN     "userId" INTEGER NOT NULL,
+ADD CONSTRAINT "UserPreference_pkey" PRIMARY KEY ("id");
+
+-- CreateTable
+CREATE TABLE "Image" (
+    "id" SERIAL NOT NULL,
+    "url" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Image_userId_key" ON "Image"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_nickname_key" ON "User"("nickname");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserPreference_userId_key" ON "UserPreference"("userId");
+
+-- AddForeignKey
+ALTER TABLE "UserPreference" ADD CONSTRAINT "UserPreference_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Image" ADD CONSTRAINT "Image_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_articleId_fkey" FOREIGN KEY ("articleId") REFERENCES "Article"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
